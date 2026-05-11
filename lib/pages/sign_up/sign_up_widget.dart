799dd6f3e74b38912c52ff38e6c1411c737bf3ae
promagician77 <bluesky429311@gmail.com>
2026-05-11 03:23:52 -0400

    feat: add device_info table

--- lib/pages/sign_up/sign_up_widget.dart
+++ lib/pages/sign_up/sign_up_widget.dart
@@ -375,6 +375,9 @@ class _SignUpWidgetState extends State<SignUpWidget> {
             await SupabaseService.ensureUserProfileFromAuth(overrideName: name);
           } catch (_) {}
           try {
+            await SupabaseService.upsertDeviceInfoForCurrentUser();
+          } catch (_) {}
+          try {
             await TimezoneSyncService.syncIfNeeded();
           } catch (_) {}
         });
